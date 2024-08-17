46a416c355c570f7eaae5469826887d231354016
Teun Jansen <teunjansen91@gmail.com>
2024-08-17 08:33:05 +0200

    Ready to start building the app.

---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+A Patient Management System build by Teun Jansen
+Next.js | Twillio | Typescript | TailwindCSS | Shadcn
+
+start with: npm run dev
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl underline">Home</h1>;
+  return (
+    <div>
+      <h1 className="text-white text-3xl underline">Home</h1>
+      <Button>Click me!</Button>
+    </div>
+  );
 }
